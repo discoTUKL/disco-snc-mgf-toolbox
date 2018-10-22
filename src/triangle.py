3d7340fe31df570514472810b158ee04d9d979fc
Paul Nikolaus <nikolaus@cs.uni-kl.de>
2018-10-22 19:01:35 +0100

    server bound

--- src/triangle.py
+++ src/triangle.py
@@ -7,56 +7,48 @@ import numpy as np
 import scipy.optimize
 
 from library.exceptions import ParameterOutOfBounds
-from library.helper_functions import mgf
+from library.perform_parameter import PerformEnum, PerformParameter
+from nc_operations.evaluate_single_hop import evaluate_single_hop
 from nc_operations.operations import Convolve, Deconvolve, Leftover
-from nc_operations.performance_bounds import delay_prob
 from nc_processes.arrival import Arrival
 from nc_processes.arrival_distribution import DM1, ArrivalDistribution
 from nc_processes.constant_rate_server import ConstantRate
+from nc_processes.regulated_arrivals import TokenBucketConstant
 from nc_processes.service import Service
 
 
-def new_delay(theta: float, tt: int, delay: int, a_1: ArrivalDistribution,
-              a_2: ArrivalDistribution, a_3: ArrivalDistribution,
-              s_1: ConstantRate, s_3: ConstantRate) -> float:
-    sigmas = a_1.sigma(theta=theta) + a_2.sigma(theta=theta) + a_3.sigma(
-        theta=theta) + s_1.sigma(theta=theta) + s_3.sigma(theta=theta)
+def new_delay(theta: float, delay: int, a_1: ArrivalDistribution,
+              a_2: ArrivalDistribution, s_1: ConstantRate,
+              s_2: TokenBucketConstant, s_3: ConstantRate) -> float:
+    s_net: Service = Convolve(
+        ser1=Leftover(arr=a_2, ser=s_1),
+        ser2=Leftover(arr=s_2, ser=s_3),
+        indep=True)
 
-    outer = 0.0
-    for k in range(tt + 1):
-        inner = 0.0
-        for i in range(k, tt + delay + 1):
-            inner += mgf(
-                theta=theta,
-                x=s_1.rho(theta=theta) * (i - k) + a_2.rho(theta=theta) *
-                (i - k) + s_3.rho(theta=theta) * (tt - i))
-
-        outer += mgf(theta=theta, x=a_1.rho(theta=theta) * (tt - k)) * inner
-
-    return mgf(
+    return evaluate_single_hop(
+        foi=a_1,
+        s_net=s_net,
         theta=theta,
-        x=a_3.rho(theta=theta) *
-        (tt + delay) + s_3.rho(theta=theta) * delay + sigmas) * outer
+        perform_param=PerformParameter(
+            perform_metric=PerformEnum.DELAY_PROB, value=delay))
 
 
-def new_delay_opt(tt: int,
-                  delay: int,
+def new_delay_opt(delay: int,
                   a_1: ArrivalDistribution,
                   a_2: ArrivalDistribution,
-                  a_3: ArrivalDistribution,
                   s_1: ConstantRate,
+                  s_2: TokenBucketConstant,
                   s_3: ConstantRate,
                   print_x=False) -> float:
-    def helper_fun(param: float) -> float:
+    def helper_fun(param_list: List[float]) -> float:
         try:
             return new_delay(
-                theta=param,
-                tt=tt,
+                theta=param_list[0],
                 delay=delay,
                 a_1=a_1,
                 a_2=a_2,
-                a_3=a_3,
                 s_1=s_1,
+                s_2=s_2,
                 s_3=s_3)
         except (FloatingPointError, OverflowError, ParameterOutOfBounds):
             return inf
@@ -89,8 +81,12 @@ def standard_delay(theta: float, p: float, delay: int,
         indep=False,
         p=p)
 
-    return delay_prob(
-        arr=a_1, ser=s_net, theta=theta, delay_value=delay, indep=True)
+    return evaluate_single_hop(
+        foi=a_1,
+        s_net=s_net,
+        theta=theta,
+        perform_param=PerformParameter(
+            perform_metric=PerformEnum.DELAY_PROB, value=delay))
 
 
 def standard_delay_opt(delay: int,
@@ -135,35 +131,36 @@ def standard_delay_opt(delay: int,
 
 
 if __name__ == '__main__':
-    TT = 20
     DELAY = 5
     A_1 = DM1(lamb=1.0, n=1)
     A_2 = DM1(lamb=1.0, n=1)
     A_3 = DM1(lamb=1.0, n=1)
     S_1 = ConstantRate(rate=6.0)
-    S_2 = ConstantRate(rate=6.0)
     S_3 = ConstantRate(rate=6.0)
+    for i in range(7):
+        S_2 = TokenBucketConstant(sigma_single=0.0, rho_single=i, n=1)
+        print("rate", i)
+        print(
+            "new bound",
+            new_delay_opt(
+                delay=DELAY,
+                a_1=A_1,
+                a_2=A_2,
+                s_1=S_1,
+                s_2=S_2,
+                s_3=S_3,
+                print_x=False))
 
-    print(
-        "new bound",
-        new_delay_opt(
-            tt=TT,
-            delay=DELAY,
-            a_1=A_1,
-            a_2=A_2,
-            a_3=A_3,
-            s_1=S_1,
-            s_3=S_3,
-            print_x=False))
+        S_2 = ConstantRate(rate=i)
 
-    print(
-        "standard bound",
-        standard_delay_opt(
-            delay=DELAY,
-            a_1=A_1,
-            a_2=A_2,
-            a_3=A_3,
-            s_1=S_1,
-            s_2=S_2,
-            s_3=S_3,
-            print_x=False))
+        print(
+            "standard bound",
+            standard_delay_opt(
+                delay=DELAY,
+                a_1=A_1,
+                a_2=A_2,
+                a_3=A_3,
+                s_1=S_1,
+                s_2=S_2,
+                s_3=S_3,
+                print_x=False))
