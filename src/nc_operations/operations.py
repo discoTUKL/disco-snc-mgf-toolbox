e00bf0af13b15fbc20075e1c4b301b574f488599
Paul Nikolaus <nikolaus@cs.uni-kl.de>
2020-07-13 15:29:33 +0200

    small improvements for AggregateList()

--- src/nc_operations/operations.py
+++ src/nc_operations/operations.py
@@ -3,12 +3,14 @@
 from math import exp, log
 from typing import List
 
+import numpy as np
+
 from nc_arrivals.arrival import Arrival
 from nc_arrivals.regulated_arrivals import DetermTokenBucket
 from nc_operations.stability_check import stability_check
 from nc_server.rate_latency_server import RateLatencyServer
 from nc_server.server import Server
-from utils.exceptions import ParameterOutOfBounds
+from utils.exceptions import IllegalArgumentError, ParameterOutOfBounds
 from utils.helper_functions import get_p_n, get_q, is_equal
 
 
@@ -192,22 +194,23 @@ class ConvolveAlter(Server):
 
 class AggregateList(Arrival):
     """Multiple (list) aggregation class."""
-    def __init__(self,
-                 arr_list: List[Arrival],
-                 p_list: List[float],
-                 indep=True) -> None:
-        self.arr_list = arr_list
+    def __init__(self, arr_list: List[Arrival], indep: bool,
+                 p_list: List[float]) -> None:
+        self.arr_list = arr_list[:]
         if indep:
             self.p_list = [1.0]
             # self.p_n = 1.0
         else:
             if len(p_list) != (len(self.arr_list) - 1):
-                raise ValueError(
+                raise IllegalArgumentError(
                     f"number of p={len(p_list)} and length of "
                     f"arr_list={len(self.arr_list)} - 1 have to match")
 
-            self.p_list = p_list
-            self.p_n = get_p_n(p_list=p_list)
+            self.p_list = p_list[:]
+            if isinstance(p_list, np.ndarray):
+                np.concatenate((self.p_list, get_p_n(p_list=p_list)))
+            else:
+                self.p_list.append(get_p_n(p_list=p_list))
         self.indep = indep
 
     def sigma(self, theta: float) -> float:
@@ -216,10 +219,8 @@ class AggregateList(Arrival):
             for arr in self.arr_list:
                 res += arr.sigma(theta)
         else:
-            for i in range(len(self.arr_list) - 1):
-                res += self.arr_list[i].sigma(self.p_list[i] * theta)
-
-            res += self.arr_list[-1].sigma(self.p_n * theta)
+            for i, arr in enumerate(self.arr_list):
+                res += arr.sigma(self.p_list[i] * theta)
 
         return res
 
@@ -235,18 +236,12 @@ class AggregateList(Arrival):
                 res += rho_i
 
         else:
-            for i in range(len(self.arr_list) - 1):
-                rho_i = self.arr_list[i].rho(self.p_list[i] * theta)
+            for i, arr in enumerate(self.arr_list):
+                rho_i = arr.rho(self.p_list[i] * theta)
                 if rho_i < 0:
                     raise ParameterOutOfBounds("The rhos must be >= 0")
 
                 res += rho_i
-
-            rho_n = self.arr_list[-1].rho(self.p_n * theta)
-            if rho_n < 0:
-                raise ParameterOutOfBounds("The rhos must be >= 0")
-
-            res += rho_n
 
         return res
 
@@ -295,7 +290,8 @@ class AggregateHomogeneous(Arrival):
         self.n = n
 
         if not indep:
-            raise NotImplementedError
+            raise NotImplementedError(
+                "only the independet case is implemented")
 
     def sigma(self, theta: float) -> float:
         return self.n * self.arr.sigma(theta=theta)
@@ -309,24 +305,27 @@ class AggregateHomogeneous(Arrival):
 
 if __name__ == '__main__':
     from timeit import default_timer as timer
-    from nc_arrivals.regulated_arrivals import DetermTokenBucket
+
     ARR_LIST = [
         DetermTokenBucket(sigma_single=1.0, rho_single=1.5, n=8),
         DetermTokenBucket(sigma_single=2.0, rho_single=3.0, n=10)
     ]
 
     START = timer()
-    AGG_LIST = AggregateList(arr_list=ARR_LIST, p_list=[], indep=True)
+    AGG_LIST = AggregateList(arr_list=ARR_LIST, indep=False, p_list=[2.0])
     STOP = timer()
     TIME_LIST = STOP - START
     START = timer()
-    AGG_TWO = AggregateTwo(arr1=ARR_LIST[0], arr2=ARR_LIST[1], indep=True)
+    AGG_TWO = AggregateTwo(arr1=ARR_LIST[0],
+                           arr2=ARR_LIST[1],
+                           indep=False,
+                           p=2.0)
     STOP = timer()
     TIME_TWO = STOP - START
 
     print(f"sum sigma list = {AGG_LIST.sigma(theta=1.0)}")
-    print(f"sum rho list = {AGG_LIST.rho(theta=1.0)}")
     print(f"time sigma two = {AGG_TWO.sigma(theta=1.0)}")
+    print(f"sum rho list = {AGG_LIST.rho(theta=1.0)}")
     print(f"time rho two = {AGG_TWO.rho(theta=1.0)}")
 
     print(f"time list = {TIME_LIST} s")
