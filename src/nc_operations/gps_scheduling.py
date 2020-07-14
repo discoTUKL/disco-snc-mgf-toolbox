1f4a0c3c5544f010c5421d3f10586134aafbbec6
Paul Nikolaus <nikolaus@cs.uni-kl.de>
2020-07-14 15:47:06 +0200

    LeftoverGPSPG()

--- src/nc_operations/gps_scheduling.py
+++ src/nc_operations/gps_scheduling.py
@@ -4,20 +4,13 @@ from typing import List
 
 from nc_arrivals.arrival import Arrival
 from nc_server.server import Server
-from utils.exceptions import IllegalArgumentError, ParameterOutOfBounds
+from utils.exceptions import ParameterOutOfBounds
 
 
 class LeftoverGPSPG(Server):
     """Class to compute the leftover service for GPS."""
-    def __init__(self, ser: Server, arr_list: List[Arrival],
-                 phi_list: List[float]) -> None:
+    def __init__(self, ser: Server, phi_list: List[float]) -> None:
         self.ser = ser
-        self.arr_list = arr_list
-
-        if len(arr_list) != len(phi_list):
-            raise IllegalArgumentError(f"len(arr_list) and len(phi_list) have "
-                                       f"to match")
-
         self.phi_foi_weight = phi_list[0] / sum(phi_list)
 
     def sigma(self, theta):
