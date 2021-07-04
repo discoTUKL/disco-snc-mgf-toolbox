dd4355ebf35fed6c14a2442f3103cd0cdbe1b375
Paul Nikolaus <nikolaus@cs.uni-kl.de>
2021-07-04 19:51:37 +0200

    improved performance bounds

--- src/nc_operations/performance_bounds.py
+++ src/nc_operations/performance_bounds.py
@@ -1,6 +1,5 @@
 """Performance bounds"""
 
-import warnings
 from math import exp, inf, log
 
 from nc_arrivals.arrival import Arrival
@@ -17,7 +16,7 @@ def backlog_prob(arr: Arrival,
                  backlog_value: float,
                  indep=True,
                  p=1.0,
-                 geom_series=True) -> float:
+                 geom_series=False) -> float:
     """Implements stationary standard_bound method"""
     if indep:
         p = 1.0
@@ -40,20 +39,10 @@ def backlog_prob(arr: Arrival,
                     theta * sigma_sum) / (-rho_diff * theta)
             else:
                 tau_opt = 1 / (theta * ser.rho(theta=q * theta))
-                opt_res = exp(-theta * backlog_value) * exp(
+                return exp(-theta * backlog_value) * exp(
                     theta *
                     (ser.rho(theta=q * theta) * tau_opt + sigma_sum)) / (
-                        -rho_diff * theta)
-
-                tau_1 = 1.0
-                one_res = exp(-theta * backlog_value) * exp(
-                    theta * (ser.rho(theta=q * theta) * tau_1 + sigma_sum)) / (
-                        -rho_diff * theta)
-
-                if one_res < opt_res:
-                    warnings.warn("tau_opt yields a worse result than tau_1")
-
-                return min(opt_res, one_res)
+                        -rho_diff * theta * tau_opt)
 
         if arr.is_discrete():
             return exp(-theta * backlog_value) * exp(
@@ -61,19 +50,9 @@ def backlog_prob(arr: Arrival,
         else:
             tau_opt = log(arr.rho(theta=p * theta) /
                           ser.rho(theta=q * theta)) / (theta * rho_diff)
-            opt_res = exp(-theta * backlog_value) * exp(
+            return exp(-theta * backlog_value) * exp(
                 theta * (arr.rho(theta=p * theta) * tau_opt + sigma_sum)) / (
                     1 - exp(theta * tau_opt * rho_diff))
-
-            tau_1 = 1.0
-            one_res = exp(-theta * backlog_value) * exp(
-                theta * (arr.rho(theta=p * theta) * tau_1 + sigma_sum)) / (
-                    1 - exp(theta * tau_1 * rho_diff))
-
-            if one_res < opt_res:
-                warnings.warn("tau_opt yields a worse result than tau_1")
-
-            return min(opt_res, one_res)
 
     except ZeroDivisionError:
         return inf
@@ -85,7 +64,7 @@ def backlog(arr: Arrival,
             prob_b: float,
             indep=True,
             p=1.0,
-            geom_series=True) -> float:
+            geom_series=False) -> float:
     """Implements stationary standard_bound method"""
     if prob_b < 0.0 or prob_b > 1.0:
         raise IllegalArgumentError(f"prob_b={prob_b} must be in (0,1)")
@@ -110,18 +89,8 @@ def backlog(arr: Arrival,
         else:
             tau_opt = 1 / (theta * ser.rho(theta=q * theta))
             log_part = log(prob_b * theta * tau_opt * (-rho_diff))
-            opt_res = tau_opt * ser.rho(theta=q *
-                                        theta) + sigma_sum - log_part / theta
-
-            tau_1 = 1.0
-            log_part = log(prob_b * theta * tau_1 * (-rho_diff))
-            one_res = tau_1 * ser.rho(theta=q *
-                                      theta) + sigma_sum - log_part / theta
-
-            if one_res < opt_res:
-                warnings.warn("tau_opt yields a worse result than tau_1")
-
-            return min(opt_res, one_res)
+            return tau_opt * ser.rho(theta=q *
+                                     theta) + sigma_sum - log_part / theta
 
     if arr.is_discrete():
         log_part = log(prob_b * (1 - exp(theta * rho_diff)))
@@ -130,18 +99,8 @@ def backlog(arr: Arrival,
         tau_opt = log(arr.rho(theta=p * theta) /
                       ser.rho(theta=q * theta)) / (theta * rho_diff)
         log_part = log(prob_b * (1 - exp(theta * tau_opt * rho_diff)))
-        opt_res = tau_opt * arr.rho(theta=p *
-                                    theta) + sigma_sum - log_part / theta
-
-        tau_1 = 1.0
-        log_part = log(prob_b * (1 - exp(theta * tau_1 * rho_diff)))
-        one_res = tau_1 * arr.rho(theta=p *
-                                  theta) + sigma_sum - log_part / theta
-
-        if one_res < opt_res:
-            warnings.warn("tau_opt yields a worse result than tau_1")
-
-        return min(opt_res, one_res)
+        return tau_opt * arr.rho(theta=p *
+                                 theta) + sigma_sum - log_part / theta
 
 
 def delay_prob(arr: Arrival,
@@ -150,7 +109,7 @@ def delay_prob(arr: Arrival,
                delay_value: int,
                indep=True,
                p=1.0,
-               geom_series=True) -> float:
+               geom_series=False) -> float:
     """Implements stationary standard_bound method"""
     if indep:
         p = 1.0
@@ -174,25 +133,11 @@ def delay_prob(arr: Arrival,
                         theta * sigma_sum) / (-rho_diff * theta)
             else:
                 tau_opt = 1 / (theta * ser.rho(theta=q * theta))
-                opt_res = exp(
+                return exp(
                     -theta * ser.rho(theta=q * theta) * delay_value) * exp(
                         theta *
                         (ser.rho(theta=q * theta) * tau_opt + sigma_sum)) / (
                             -rho_diff * theta * tau_opt)
-
-                tau_1 = 1.0
-                one_res = exp(
-                    -theta * ser.rho(theta=q * theta) * delay_value) * exp(
-                        theta *
-                        (ser.rho(theta=q * theta) * tau_1 + sigma_sum)) / (
-                            -rho_diff * theta * tau_1)
-
-                if one_res < opt_res:
-                    # warning should not occur anymore, since missing tau -
-                    # factor has been fixed
-                    warnings.warn("tau_opt yields a worse result than tau_1")
-
-                return min(opt_res, one_res)
 
         if arr.is_discrete():
             return exp(-theta * ser.rho(theta=q * theta) * delay_value) * exp(
@@ -200,22 +145,9 @@ def delay_prob(arr: Arrival,
         else:
             tau_opt = log(arr.rho(theta=p * theta) /
                           ser.rho(theta=q * theta)) / (theta * rho_diff)
-            opt_res = exp(
-                -theta * ser.rho(theta=q * theta) * delay_value) * exp(
-                    theta *
-                    (arr.rho(theta=p * theta) * tau_opt + sigma_sum)) / (
-                        1 - exp(theta * tau_opt * rho_diff))
-
-            tau_1 = 1.0
-            one_res = exp(
-                -theta * ser.rho(theta=q * theta) * delay_value) * exp(
-                    theta * (arr.rho(theta=p * theta) * tau_1 + sigma_sum)) / (
-                        1 - exp(theta * tau_1 * rho_diff))
-
-            if one_res < opt_res:
-                warnings.warn("tau_opt yields a worse result than tau_1")
-
-            return min(opt_res, one_res)
+            return exp(-theta * ser.rho(theta=q * theta) * delay_value) * exp(
+                theta * (arr.rho(theta=p * theta) * tau_opt + sigma_sum)) / (
+                    1 - exp(theta * tau_opt * rho_diff))
 
     except ZeroDivisionError:
         return inf
@@ -227,7 +159,7 @@ def delay(arr: Arrival,
           prob_d: float,
           indep=True,
           p=1.0,
-          geom_series=True) -> float:
+          geom_series=False) -> float:
     """Implements stationary standard_bound method"""
     if prob_d < 0.0 or prob_d > 1.0:
         raise IllegalArgumentError(f"prob_b={prob_d} must be in (0,1)")
@@ -253,18 +185,8 @@ def delay(arr: Arrival,
         else:
             tau_opt = 1 / (theta * ser.rho(theta=q * theta))
             log_part = log(prob_d * theta * tau_opt * (-rho_diff))
-            opt_res = (tau_opt * ser.rho(theta=q * theta) + sigma_sum -
-                       log_part / theta) / ser.rho(theta=q * theta)
-
-            tau_1 = 1.0
-            log_part = log(prob_d * theta * tau_1 * (-rho_diff))
-            one_res = (tau_1 * ser.rho(theta=q * theta) + sigma_sum -
-                       log_part / theta) / ser.rho(theta=q * theta)
-
-            if one_res < opt_res:
-                warnings.warn("tau_opt yields a worse result than tau_1")
-
-            return min(opt_res, one_res)
+            return (tau_opt * ser.rho(theta=q * theta) + sigma_sum -
+                    log_part / theta) / ser.rho(theta=q * theta)
 
     if arr.is_discrete():
         log_part = log(prob_d * (1 - exp(theta * rho_diff)))
@@ -273,18 +195,8 @@ def delay(arr: Arrival,
         tau_opt = log(arr.rho(theta=p * theta) /
                       ser.rho(theta=q * theta)) / (theta * rho_diff)
         log_part = log(prob_d * (1 - exp(theta * tau_opt * rho_diff)))
-        opt_res = (tau_opt * arr.rho(theta=p * theta) + sigma_sum -
-                   log_part / theta) / ser.rho(theta=q * theta)
-
-        tau_1 = 1.0
-        log_part = log(prob_d * (1 - exp(theta * tau_1 * rho_diff)))
-        one_res = (tau_1 * arr.rho(theta=p * theta) + sigma_sum -
-                   log_part / theta) / ser.rho(theta=q * theta)
-
-        if one_res < opt_res:
-            warnings.warn("tau_opt yields a worse result than tau_1")
-
-        return min(opt_res, one_res)
+        return (tau_opt * arr.rho(theta=p * theta) + sigma_sum -
+                log_part / theta) / ser.rho(theta=q * theta)
 
 
 def output(arr: Arrival,
